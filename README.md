e68fc1f70d48114cbe460721b3c74512ac7d6fcb
Desiree Desario <desireedesario@gmail.com>
2016-06-28 12:11:16 -0700

    game a game i guess

--- README.md
+++ README.md
@@ -2,22 +2,51 @@ This is a basic tic tac toe game
 
 creating TicTacToe from scratch
 Set MVP:
-	- we are mkaing tic tac TicTacToe
+	- we are making tic tac TicTacToe
 	- 2 players
 	- keep score
 	- and then reset the board
 		- MVP+ turn into ninjas vs pirates
+
+STEPS
 - create a basic file structure
 - add a readme.md
 - make a git and github repo
 - render the board
- -minumum styling
+  -minimum styling with CSS
 - grab each cell, add a click handler
+  - aka we need to click on these things, therefore we should make sure each one has a click function by looping through each cell!
 - make a click modify cell
+  - aka changing the cell from nothing to display an x or an o
 - create concept of player turn
+
+    1) when making players, we want to figure out our players.
+    2) var currentPlayer = "x"
+    3) player1(x)clicks
+    4) x shows up
+    5) and then we need to switch player
+    6) if cp is x, switch to o. else switch cp to x.
+
 - link player turn with modify cell
+
+  aka make a variable with a current player
+
 - make all clickable only clickable once
+
+  this will be  conditional code that explains that if a space is empty, fill with HTML, else do not.
+
 - win logic
+
+      with this we need to create 8 winning arrays within an array
+
+      we must check all possibilities
+      then we need to irerate over the winners with a for() loop
+
+      EX: one winning combination:
+        check if cell0 == cell1 && cell0 == cell2
+        winner = cell0
+        else no winner
+
 - ties
 - keep score
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,78 @@
 console.log("Ready!")
+
+//first grab all of our tile pieces
+var tiles = document.getElementsByClassName('tile')
+//console.log(tiles)
+
+//current player
+var currentPlayer = "X";
+var clicked;
+
+for (var i = 0; i < tiles.length; i++){
+  //prints out each tile individually
+    //console.log(tiles[i])
+  // this will show that each element on the dom is being clicked
+  tiles[i].addEventListener('click', function(){
+    //console.log("You clicked: " + this);
+  //if we used this.id = 'clicked', it will change each event you clicked and turn the id into clicked
+    //this.id = 'clicked';
+
+    //NOW we need to figure out how to switch players
+    //AND we need to make that cell un clickable
+    if (this.innerHTML == "") { //if it is not empty
+      //when a user clicks, we want to show that an x or o is placed there
+        this.innerHTML = currentPlayer
+        //sweet! made an x! but these are small x sizes. change the find size on your tile class to bump up that size!
+        checkWin();
+      switchPlayer();
+    } else {
+      //change this not to be an alert. maybe have a message div or append a message div.
+      alert("Hey! Listen! You need to click an empty cell!");
+    }
+  })
+}
+
+//SWITCHING A PLAYER FUNCTION
+function switchPlayer(){
+  if (currentPlayer == "X") {
+    currentPlayer = "O";
+  } else {
+    currentPlayer = "X";
+  }
+}
+
+//WINNING
+
+// with this we need to create 8 winning arrays within an array
+
+var winningCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+
+//because there is not a winner yet it is false
+var winner = "";
+
+function checkWin(){
+  for (var i = 0; i < winningCombos.length; i++){
+    // var a = document.getElementById("cell"+ winningCombos[i][0])
+    // var b = document.getElementById("cell"+ winningCombos[i][1])
+    // var c = document.getElementById("cell"+ winningCombos[i][2])
+    var currentArray = winningCombos[i];
+    var a = document.getElementById("cell"+ currentArray[0]).innerHTML;
+    var b = document.getElementById("cell"+ currentArray[1]).innerHTML;
+    var c = document.getElementById("cell"+ currentArray[2]).innerHTML;
+    //if a and c match, and a an b won, AND SPACES ARE NOT EMPTY, SOMEONE WON
+    if (a == b && a == c && a!=""){
+      // winner = a;
+      console.log("someone won!", a);
+    }
+
+  }
+}
